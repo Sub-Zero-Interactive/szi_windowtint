ef13a4a008c5777aad9a72996a59c8e54d66466e
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-11-07 06:32:39 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -117,18 +117,6 @@ RegisterNUICallback("exit", function(data)
     })
 end)
 
-Citizen.CreateThread(function()
-    while display do
-        Citizen.Wait(0)
-        DisableControlAction(0, 1, display) -- LookLeftRight
-        DisableControlAction(0, 2, display) -- LookUpDown
-        DisableControlAction(0, 142, display) -- MeleeAttackAlternate
-        DisableControlAction(0, 18, display) -- Enter
-        DisableControlAction(0, 322, display) -- ESC
-        DisableControlAction(0, 106, display) -- VehicleMouseControlOverride
-    end
-end)
-
 RegisterNetEvent('szi_windowtint:displayTint')
 AddEventHandler('szi_windowtint:displayTint', function(tint, color)
     SendNUIMessage({
